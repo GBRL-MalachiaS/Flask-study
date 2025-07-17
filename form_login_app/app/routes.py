cbe0251677b6c1df8ec6b50b42d806d10a4e0c3c
Gabriel Malachias Hangrad <gbl.malachias@gmail.com>
2025-07-17 16:10:16 -0300

    update

--- form_login_app/app/routes.py
+++ form_login_app/app/routes.py
@@ -3,11 +3,15 @@ from app import app
 from app.forms import FormLogin, FormCadastro
 
 usuarios = {
-    "gabriel": "minhasenha"
+    1: {
+        'Nome': 'Gabriel',
+        'Senha': 'Lorena@2402.',
+        'e-mail': 'gbl.malachias@gmail.com'
+    }
 }
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def login():
     form = FormLogin()
 
@@ -16,21 +20,47 @@ def login():
         login = form.usuario.data
         senha = form.senha.data
 
-        if login in usuarios:
-            if usuarios[login.lower()] == senha:
-                flash(
-                    f'Login solicitado para o usuário {form.usuario.data}, Lembrar-me={form.lembrar_me.data}')
-                return redirect(url_for('logado'))
-            else:
-                flash("Usuário ou senha inválidos!")
-                return render_template("login.html", form=form)
+        for id, usuario in usuarios.items():
+            if usuario['Nome'] == login and usuario['Senha'] == senha:
+                flash('Login foi validado! ')
+                return redirect({url_for('/logado')})
 
+        flash('Usuario invalido!')
+        return render_template('login.html', titulo='Login', form=form)
     return render_template('login.html', titulo='Login', form=form)
 
-@app.route('/cadastro')
+
+@app.route('/cadastro', methods=['GET','POST'])
 def cadastro():
     form = FormCadastro()
+
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        senha = form.senha.data
+        senha_confirmacao = form.senha_confirmacao.data
+
+        for id, usuario in usuarios.items():
+
+            if nome == usuario['Nome']:
+                flash('Usuário já possui cadastro')
+                return render_template('cadastro.html', titulo='Cadastro', form=form)
+        
+        if senha == senha_confirmacao:
+            usuarios.update({
+                len(usuarios):{
+                    'Nome':nome,
+                    'E-mail':email,
+                    'Senha':senha
+                }})
+            flash(f'Usuário {nome} foi cadastrado! ')
+            return redirect(url_for('logado'))
+        else:
+            flash('Senha não batem!')
+            
+
     return render_template('cadastro.html', titulo='Cadastro', form=form)
+
 
 @app.route('/logado')
 def logado():
